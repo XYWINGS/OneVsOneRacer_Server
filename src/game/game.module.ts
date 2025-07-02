ef15c753b93eba0480ce994128eee18fb642fbeb
XYWINGS <chalithbandara99@gmail.com>
2025-07-02 20:48:27 +0530

    feat: add LoggerMiddleware for HTTP request logging and enhance GameModule configuration

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GameGateway } from './game/game.gateway';
 import { GameService } from './game/game.service';
+import { LoggerMiddleware } from 'src/logger/logger.middleware';
 
 @Module({
   providers: [GameGateway, GameService],
 })
-export class GameModule {}
+export class GameModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { GameModule } from './game/game.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+  const port = process.env.PORT ?? 3000;
+
   const app = await NestFactory.create(GameModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(port);
+  logger.log(`Game server is running on http://localhost:${port}`);
 }
 bootstrap();
